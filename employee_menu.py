01bd701633a310716a8c822bf2bf246c71c7bc27
IndreLuko <indre.jaruseviciute@gmail.com>
2021-11-24 13:19:30 +0200

    updated menu validation

--- employee_menu.py
+++ employee_menu.py
@@ -15,7 +15,9 @@ employee_manager = EmployeeManager()
 while selection != 0:
     selection = Employee.selection()
     if selection == 1:
-        employee_manager.hire_employee(Employee.selection_name(), Employee.selection_surname(), Employee.selection_age(), Employee.selection_employment_date(), Employee.selection_day_of_leaving(), Employee.selection_phone_number())
+        employee_manager.hire_employee(Employee.selection_name(), Employee.selection_surname(),
+                                       Employee.selection_age(), Employee.selection_employment_date(),
+                                       Employee.selection_day_of_leaving(), Employee.selection_phone_number())
     elif selection == 2:
         employee_manager.list()
     elif selection == 3:
